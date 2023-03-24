4a7c897de7a6836838a05ceb2a86512f829ed48d
wgao24 <wgao24@choate.edu>
2023-03-24 08:17:30 -0400

    kermit

--- motor.py
+++ motor.py
@@ -4,6 +4,9 @@ class Motor:
         self.speed = .5
 
     def setSpeed(self,speed):
+        speed = max(-1, speed)
+        speed = min( 1, speed)
+        speed = speed^3##making a deadzone
         self.speed = speed
     def speedUp(self):
         self.speed = self.speed * 2
